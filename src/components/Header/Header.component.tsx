d2366b087fdff38764e1036d16cee1495dd63018
HernanGabriel <hernan24744@gmail.com>
2023-07-09 17:40:34 -0300

    se agrega maquetado del header implementando react-bootstrap

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,4 +1,23 @@
+// libreria ract-bootstrap
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+
 const Header = () => {
-  return <h1></h1>;
+  return (
+    <>
+      <Navbar className="bg-dark" expand="lg" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className="text-light">
+                00:20
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 export default Header;
