6d7caeec000ca12915df11575d85ce7c83717aa8
HernanGabriel <hernan24744@gmail.com>
2023-07-09 17:50:58 -0300

    implementacion de useContex para el manejo de estado

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -1,4 +1,25 @@
+import React, { useContext, useEffect } from "react";
+
+//COMPONENTES
+import Header from "../Header/Header.component";
+import Home from "../Home/Home.component";
+import Cuestionario from "../Cuestionario/Cuestionario.component";
+import Footer from "../Footer/Footer.component";
+//CONTEXT
+import { CuestionarioContext } from "../../context/Cuestionario.context";
+
 const Layout = () => {
-  return <h1></h1>;
+  //implementacion del context
+  const [estado, dispatch] = useContext(CuestionarioContext);
+
+  return (
+    <>
+      <Header />
+      {estado.estadoJuego === "Empezar" && <Home />}
+      {estado.estadoJuego === "Jugando" && <Cuestionario />}
+      {/* {estado.estadoJuego === "Finalizado" && } */}
+      <Footer />
+    </>
+  );
 };
 export default Layout;
