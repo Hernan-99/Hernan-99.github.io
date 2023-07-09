5b32bb8477b2d3ce4a9bd9d34b020f962b24a235
HernanGabriel <hernan24744@gmail.com>
2023-07-09 17:41:11 -0300

    se agrega maquetado del footer implementando react-bootstrap

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,4 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 const Footer = () => {
-  return <h1></h1>;
+  return (
+    <footer className="bg-dark text-light py-3">
+      <Container>
+        <Row>
+          <Col xs={12} className="text-center">
+            <h5>Footer App</h5>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 export default Footer;
