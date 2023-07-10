b507830602f5e6e7f63a5f01904daf0ee8bed02e
HernanGabriel <hernan24744@gmail.com>
2023-07-10 07:47:27 -0300

    link para visitar el repositorio

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -10,8 +10,12 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className="text-light">
-                00:20
+              <Nav.Link
+                href="https://github.com/Hernan-99/Hernan-99.github.io"
+                target="_blank"
+                className="text-light"
+              >
+                Visitar repositorio
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
