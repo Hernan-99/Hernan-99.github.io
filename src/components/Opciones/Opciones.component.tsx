ff03b4c2a8b6f305d24dbc22b22dd04bf3b49f37
HernanGabriel <hernan24744@gmail.com>
2023-07-10 07:53:55 -0300

    Actualizacion del componente opciones. Implementacion de logica para mostrar la respuesta correcta y aplicar clases de acuerdo a la opcion correcta

--- src/components/Opciones/Opciones.component.tsx
+++ src/components/Opciones/Opciones.component.tsx
@@ -1,4 +1,48 @@
-const Opciones = () => {
-  return <h1></h1>;
+import { useContext } from "react";
+import { CuestionarioContext } from "../../context/Cuestionario.context";
+
+import "../../assets/styles/Opciones.style.css";
+
+type OpcionesProps = {
+  opcion: string;
+  respuestaCorrecta: string;
+  opSelec: () => void;
+  seleccionada: boolean;
 };
-export default Opciones;
+const Opcion: React.FC<OpcionesProps> = ({
+  opcion,
+  respuestaCorrecta,
+  opSelec,
+}) => {
+  const [cuestionarioEstado, dispatch] = useContext(CuestionarioContext);
+
+  const handleClick = () => {
+    opSelec();
+
+    dispatch({
+      type: "CHECK_ANSWER",
+      payload: {
+        respuestaCorrecta: respuestaCorrecta,
+        opcion: opcion,
+      },
+    });
+  };
+
+  return (
+    <div
+      className={`opcion ${
+        cuestionarioEstado.respuestaSeleccionada && opcion === respuestaCorrecta
+          ? "correcto"
+          : ""
+      } ${
+        cuestionarioEstado.respuestaSeleccionada && opcion !== respuestaCorrecta
+          ? "incorrecto"
+          : ""
+      }`}
+      onClick={handleClick}
+    >
+      <p>{opcion}</p>
+    </div>
+  );
+};
+export default Opcion;
