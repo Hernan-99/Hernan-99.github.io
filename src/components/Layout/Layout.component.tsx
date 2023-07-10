64689a3a765f755c411b18d6b4aa0c02c3fdc874
HernanGabriel <hernan24744@gmail.com>
2023-07-10 07:38:14 -0300

    Layout actualizado. implementacion de useContext y useEffect

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -4,20 +4,26 @@ import React, { useContext, useEffect } from "react";
 import Header from "../Header/Header.component";
 import Home from "../Home/Home.component";
 import Cuestionario from "../Cuestionario/Cuestionario.component";
+import EndGame from "../EndGame/EndGame.component";
 import Footer from "../Footer/Footer.component";
 //CONTEXT
 import { CuestionarioContext } from "../../context/Cuestionario.context";
 
 const Layout = () => {
   //implementacion del context
-  const [estado, dispatch] = useContext(CuestionarioContext);
+  const [cuestionarioEstado, dispatch] = useContext(CuestionarioContext);
+
+  useEffect(() => {
+    dispatch({ type: "RANDOM_PREGUNTAS" });
+  }, []);
 
   return (
+    //Renderizacion del componente de acuerdo al estado
     <>
       <Header />
-      {estado.estadoJuego === "Empezar" && <Home />}
-      {estado.estadoJuego === "Jugando" && <Cuestionario />}
-      {/* {estado.estadoJuego === "Finalizado" && } */}
+      {cuestionarioEstado.estadoJuego === "Empezar" && <Home />}
+      {cuestionarioEstado.estadoJuego === "Jugando" && <Cuestionario />}
+      {cuestionarioEstado.estadoJuego === "Finalizado" && <EndGame />}
       <Footer />
     </>
   );
